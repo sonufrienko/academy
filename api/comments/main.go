d7ed40b57790c6583a00d7d17b2264d5c0f896f2
Sergey Onufrienko <sergey@onufrienko.com>
2021-04-04 11:08:01 +0300

    Refactoring

--- api/comments/main.go
+++ api/comments/main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/sonufrienko/academy/api/comments/routes"
 )
 
 func main() {
 	app := fiber.New()
-	app.Get("/comments", func(c *fiber.Ctx) error {
-		return c.SendString("Comments goes here")
-	})
 
-	app.Listen(":3000")
+	v1 := app.Group("/v1/comments")
+	v1.Get("/", routes.GetComments)
+	v1.Post("/", routes.AddComments)
+	v1.Delete("/:id", routes.DeleteComments)
+
+	log.Fatal(app.Listen(":3000"))
 }
