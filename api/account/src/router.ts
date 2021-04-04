adc1889ab4ed092d8e47df527e092c308402e505
Sergey Onufrienko <sergey@onufrienko.com>
2021-04-04 15:21:52 +0300

    Clan up

--- api/account/src/router.ts
+++ api/account/src/router.ts
@@ -12,34 +12,6 @@ accountRouter.get('/', async (req: Request, res: Response) => {
   }
 });
 
-// accountRouter.get('/:id', async (req: Request, res: Response) => {
-//   const id: number = parseInt(req.params.id, 10);
-
-//   try {
-//     const item: Item = await ItemService.find(id);
-
-//     if (item) {
-//       return res.status(200).send(item);
-//     }
-
-//     res.status(404).send('item not found');
-//   } catch (e) {
-//     res.status(500).send(e.message);
-//   }
-// });
-
-// accountRouter.post('/', async (req: Request, res: Response) => {
-//   try {
-//     const item: BaseItem = req.body;
-
-//     const newItem = await ItemService.create(item);
-
-//     res.status(201).json(newItem);
-//   } catch (e) {
-//     res.status(500).send(e.message);
-//   }
-// });
-
 accountRouter.put('/', async (req: Request, res: Response) => {
   try {
     const data = await accountService.update('test-user-id', req.body);
@@ -49,14 +21,3 @@ accountRouter.put('/', async (req: Request, res: Response) => {
     res.status(500).send(e.message);
   }
 });
-
-// accountRouter.delete('/:id', async (req: Request, res: Response) => {
-//   try {
-//     const id: number = parseInt(req.params.id, 10);
-//     await ItemService.remove(id);
-
-//     res.sendStatus(204);
-//   } catch (e) {
-//     res.status(500).send(e.message);
-//   }
-// });
