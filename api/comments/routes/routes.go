e1f2a67bbb88953d07609d28c8d331182115ecf9
Sergey Onufrienko <sergey@onufrienko.com>
2021-04-04 11:07:45 +0300

    Added routes package

---
+++ api/comments/routes/routes.go
@@ -0,0 +1,45 @@
+package routes
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/sonufrienko/academy/api/comments/comments"
+)
+
+func GetComments(c *fiber.Ctx) error {
+	courseId := c.Query("courseId")
+
+	comments, err := comments.GetComments(courseId)
+	if err != nil {
+		return err
+	}
+
+	c.JSON(comments)
+	return nil
+}
+
+func AddComments(c *fiber.Ctx) error {
+	data := new(comments.AddCommentData)
+	if err := c.BodyParser(data); err != nil {
+		return err
+	}
+
+	comment, err := comments.AddComment(*data)
+	if err != nil {
+		return err
+	}
+
+	c.JSON(comment)
+	return nil
+}
+
+func DeleteComments(c *fiber.Ctx) error {
+	commentId := c.Params("id")
+
+	err := comments.DeleteComment(commentId)
+	if err != nil {
+		return err
+	}
+
+	c.SendStatus(200)
+	return nil
+}
