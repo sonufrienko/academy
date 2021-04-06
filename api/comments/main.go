ce4c2af99f884c6aff202699cdf4b26dc2db227d
Sergey Onufrienko <sergey@onufrienko.com>
2021-04-06 21:54:45 +0300

    Added connection to MongoDB

--- api/comments/main.go
+++ api/comments/main.go
@@ -2,12 +2,31 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/sonufrienko/academy/api/comments/db"
 	"github.com/sonufrienko/academy/api/comments/routes"
 )
 
+func getEnv(key string, defaultValue string) string {
+	value, ok := os.LookupEnv(key)
+	if ok {
+		return value
+	} else {
+		return defaultValue
+	}
+}
+
 func main() {
+	connectString := getEnv("MONGO_URL", "")
+	port := getEnv("PORT", "3000")
+
+	err := db.Connect(connectString)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	app := fiber.New()
 
 	v1 := app.Group("/v1/comments")
@@ -15,5 +34,6 @@ func main() {
 	v1.Post("/", routes.AddComments)
 	v1.Delete("/:id", routes.DeleteComments)
 
-	log.Fatal(app.Listen(":3000"))
+	log.Fatal(app.Listen(":" + port))
+	defer db.Disconnect()
 }
